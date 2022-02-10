b2cdef29bfa136ccbb19ee24746ff1020d80f55d
Ryhn162004 <87838000+Ryhn162004@users.noreply.github.com>
2022-02-10 10:41:55 +0700

    add function

--- script.js
+++ script.js
@@ -6,16 +6,7 @@ $('.search-btn').on('click', function(){
             // card kosong
             let cards = '';
             movies.forEach(mov => {
-                    cards += `<div class="col-md-4 my-5">
-                    <div class="card">
-                        <img src="${mov.Poster}" class="card-img-top" alt="${mov.Poster}">
-                        <div class="card-body">
-                        <h5 class="card-title">${mov.Title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${mov.Year}</h6>
-                        <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${mov.imdbID}">Show Details</a>
-                        </div>
-                    </div>
-                </div>`; 
+                    cards += showMovies(mov); 
             });
             $('.movie-container').html(cards);
     
@@ -24,22 +15,7 @@ $('.search-btn').on('click', function(){
                 $.ajax({
                     url: 'http://www.omdbapi.com/?apikey=eb1c49d2&i=' + $(this).data('imdbid'),
                     success: mov => {
-                        const movieDetail = `<div class="container-fluid">
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <img class="img-fluid" src="${mov.Poster}" alt="${mov.Poster}">
-                                            </div>
-                                            <div class="col-md">
-                                                <ul class="list-group">
-                                                    <li class="list-group-item"><h4>${mov.Title} (${mov.Year})</h4></li>
-                                                    <li class="list-group-item"><h6>Genre : </h6>${mov.Genre}</li>
-                                                    <li class="list-group-item"><h6>Director : </h6>${mov.Director}</li>
-                                                    <li class="list-group-item"><h6>Actors : </h6>${mov.Actors}</li>
-                                                    <li class="list-group-item"><h6>Runtime : </h6>${mov.Runtime}</li>
-                                                </div>
-                                            </ul>
-                                        </div>
-                                    </div>`;
+                        const movieDetail = showMoviesDetails(mov);
                         $('.modal-body').html(movieDetail);
                     },
                     error: (err) => {
@@ -56,4 +32,37 @@ $('.search-btn').on('click', function(){
 
 });
 
+
+function showMovies(mov){
+    return `<div class="col-md-4 my-5">
+    <div class="card">
+        <img src="${mov.Poster}" class="card-img-top" alt="${mov.Poster}">
+        <div class="card-body">
+        <h5 class="card-title">${mov.Title}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${mov.Year}</h6>
+        <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${mov.imdbID}">Show Details</a>
+        </div>
+    </div>
+</div>`
+};
+
+
+function showMoviesDetails(mov){
+    return `<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+            <img class="img-fluid" src="${mov.Poster}" alt="${mov.Poster}">
+        </div>
+        <div class="col-md">
+            <ul class="list-group">
+                <li class="list-group-item"><h4>${mov.Title} (${mov.Year})</h4></li>
+                <li class="list-group-item"><h6>Genre : </h6>${mov.Genre}</li>
+                <li class="list-group-item"><h6>Director : </h6>${mov.Director}</li>
+                <li class="list-group-item"><h6>Actors : </h6>${mov.Actors}</li>
+                <li class="list-group-item"><h6>Runtime : </h6>${mov.Runtime}</li>
+            </div>
+        </ul>
+    </div>
+</div>`
+};
 
